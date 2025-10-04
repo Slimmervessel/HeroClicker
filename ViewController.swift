ea50f318d411304652d4e78ca0ba1d9ff68b5d3b
Slimmervessel <1drdarksouls@gmail.com>
2025-10-04 14:15:25 -0400

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,16 +1,5 @@
+// ViewController.swift
 import UIKit
-
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
-        return true
-    }
-}
 
 struct Upgrade {
     let name: String
